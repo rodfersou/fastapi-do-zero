aebe08d3fb6b314e8e1b26b3f56f4e3ac16fa18a
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2024-07-25 18:00:25 +0800

    Add first test

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from app.app import app
+
+
+def test_root_should_return_ok_and_hello_world():
+    client = TestClient(app)
+
+    response = client.get('/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Olar mundo!'}
