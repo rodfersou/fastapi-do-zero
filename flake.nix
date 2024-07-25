a740223ceb6914f8c710d90a69c38586d2ce34de
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2024-07-25 09:04:54 +0800

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils }:
+    flake-utils.lib.eachDefaultSystem(system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        unstable = nixpkgs-unstable.legacyPackages.${system};
+        python = unstable.python312;
+      in
+      with pkgs; {
+        devShells.default = mkShell {
+          name = "dev-environment";
+          packages = [
+            python
+            unstable.isort
+            unstable.mypy
+            unstable.poetry
+            unstable.ruff
+          ] ++ (
+            if ("$INSIDE_DOCKER" != "true") then [
+              unstable.pre-commit
+              entr
+              httpie
+              jq
+              lazygit
+              ripgrep
+              silver-searcher
+              tmux
+              tree
+            ] else [
+            ]
+          );
+          shellHook = ''
+            export INSIDE_NIX=true
+            export DEBUG=''${DEBUG:-true}
+            if [[ $INSIDE_DOCKER != "true" ]]; then
+              export POETRY_VIRTUALENVS_IN_PROJECT="true"
+            fi
+            unset PYTHONPATH
+            poetry env use ${python.executable}
+        
+            export PATH="$(poetry env info -p)/bin:$PATH"
+          '';
+        };
+      }
+    );
+}
